5f3f4e9dadbcc4e1303375827446fd6df9e86ea5
Ava Villacorta <avaviv.villa@gmail.com>
2026-07-22 10:26:08 -0700

    Changed default path to manual csv

--- code/otsu_project.py
+++ code/otsu_project.py
@@ -3,4 +3,5 @@ from validate import validate
 from auto_segment import auto_section, write_annotation
 
 if __name__ == "__main__":
-    auto_section('sect_info_with_manual.csv')
+    auto_section('../sect_info_with_manual.csv')
+
