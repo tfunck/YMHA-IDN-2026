8cfc780a369c28f5c0645bd690b93a565920b414
yaravenezuela0 <yaravenezuela0@gmail.com>
2026-07-22 10:19:22 -0700

    Add main execution block to run auto_section

--- code/otsu_project.py
+++ code/otsu_project.py
@@ -1,3 +1,6 @@
 from segment_section import segment_section
 from validate import validate
 from auto_segment import auto_section, write_annotation
+
+if __name__ == "__main__":
+    auto_section('sect_info_with_manual.csv')
