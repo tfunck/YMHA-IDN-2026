13a2aca4c384e4e8dfafca63bded46c4fdea6ee4
tupastan <simplybella671@gmail.com>
2026-07-23 10:17:55 -0700

    Update file path for auto_section function

--- code/otsu_project.py
+++ code/otsu_project.py
@@ -3,5 +3,6 @@ from validate import validate
 from auto_segment import auto_section, write_annotation
 
 if __name__ == "__main__":
-    auto_section('../sect_info_with_manual.csv')
+    auto_section('sect_info_with_manual.csv')
+
 
